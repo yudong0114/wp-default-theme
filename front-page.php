0ab2359514b4f2ebb9b1ac17cf4c2947d89bc4b9
yudong0114 <yukisaito114@gmail.com>
2021-08-17 01:16:48 +0900

    [update]TOPページ用のウィジェットの設定

--- front-page.php
+++ front-page.php
@@ -1,5 +1,11 @@
 <?php get_header(); ?>
 <article>
     <h1>TOPページ</h1>
+    <?php
+    // TOPページ用ウィジェット
+    if (is_active_sidebar('top-widget')) {
+        dynamic_sidebar('top-widget');
+    }
+    ?>
 </article>
 <?php get_footer(); ?>
--- functions.php
+++ functions.php
@@ -15,3 +15,31 @@ function setup() {
     add_editor_style( ['css/editor-style.css'] );
 }
 add_action( 'after_setup_theme', 'setup' );
+
+/**
+ * 読み込むCSSとJavaScriptを設定します。
+ */
+function common_scripts_css() {
+    // リセットCSSの読み込み
+    wp_enqueue_style('reset', get_template_directory_uri().'/css/reset.css', [], '1');
+    // サイト用CSSの読み込み
+    wp_enqueue_style('site-common', get_template_directory_uri().'/css/common.css', [], '1');
+    // サイト用スクリプトの読み込み
+    wp_enqueue_script('site-common', get_template_directory_uri().'/js/common.js', [], '1', true);
+}
+add_action( 'wp_enqueue_scripts', 'common_scripts_css' );
+
+// TOPのコンテンツをウィジェットエリア
+function top_widgets_init() {
+    register_sidebar([
+        'name' => 'TOP',
+        'id' => 'top-widget',
+        'class' => 'top-widget',
+        'description' => 'TOPのウィジェット',
+        'before_widget' => '<section id="%1$s" class="widget %2$s">',
+        'after_widget' => '</section>',
+        'before_title' => '<h2 class="widget__title">',
+        'after_title' => '</h2>',
+    ]);
+}
+add_action( 'widgets_init', 'top_widgets_init' );
