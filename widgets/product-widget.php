d42dd180291a64cf6847dd730fc7c9fe5184eff5
yudong0114 <yukisaito114@gmail.com>
2021-08-19 01:17:05 +0900

    [update]商品紹介用ウィジェットの追加

--- widgets/product-widget.php
+++ widgets/product-widget.php
@@ -3,18 +3,18 @@
  * 商品紹介用のウィジェット
  */
 class Product_Widget extends WP_Widget {
+
     /**
-     * ウィジェット名などを設定
+     * ウィジェットを登録
      */
     function __construct() {
         parent::__construct(
             'product_widget', // ID
-            __( '商品紹介' ),  // 表示名
-            [ 'description' => __( '商品紹介用ウィジェット' ), ] // 説明
+            '商品紹介', // 表示名
+            [ 'description' => '商品紹介用ウィジェット', ] // 説明
         );
     }
 
-    // ↓↓↓ 次にこちらを修正 ↓↓↓
     /**
      * ウィジェットの内容を出力.
      * 
@@ -22,38 +22,47 @@ class Product_Widget extends WP_Widget {
      * @param array $instance データベースの保存値.
      */
     public function widget( $args, $instance ){
-        echo $args['before_widget'];
-
+        // タイトルの出力
         if ( !empty( $instance['title'] ) ) {
             echo $args['before_title'] . apply_filters( 'widget_title', $instance['title'] ) . $args['after_title'];
         }
-
-        $dest_html = !empty( $instance['setting_html'] ) ? $instance['setting_html'] : '';
-        echo $dest_html;
-
-        echo $args['after_widget'];
+        // 詳細の出力
+        if ( !empty( $instance['description'] ) ) {
+            echo '<p>'.$instance['description'].'</p>';
+        }
     }
 
     /**
-     * 管理用のオプションのフォームを出力.
+     * 商品登録ウィジェットで必要なフォーム
      * 
      * @param array $instance データベースからの前回保存された値.
      */
     public function form( $instance ) {
-        $title = !empty( $instance['title'] ) ? $instance['title'] : '';
-        $current_html = !empty( $instance['setting_html'] ) ? $instance['setting_html'] : '';
-        ?>
+        // 先にデータが登録されているか
+        $title = !empty( $instance['title'] ) ? esc_attr($instance['title']) : '';
+        $description = !empty( $instance['description'] ) ? esc_attr($instance['description']) : '';
+        // フィールドIDの取得
+        $title_id = $this->get_field_id( 'title' );
+        $title_name = $this->get_field_name( 'title' );
+        // フィールドnameの取得
+        $description_id = $this->get_field_id( 'description' );
+        $description_name = $this->get_field_name( 'description' );
+
+        // フォームの出力
+        echo <<<___HTML___
         <p>
-            <label for="<?= $this->get_field_id( 'title' ); ?>"><?php _e( 'タイトル:' ); ?></label> 
-            <input class="widefat" id="<?= $this->get_field_id( 'title' ); ?>" name="<?= $this->get_field_name( 'title' ); ?>" type="text" value="<?= esc_attr( $title ); ?>">
-            <label for="<?= $this->get_field_id( 'setting_html' ); ?>"><?php _e( '表示するHTML:' ); ?></label>
-            <input id="<?= $this->get_field_id( 'setting_html' ); ?>" name="<?= $this->get_field_name( 'setting_html' ); ?>" type="text" value="<?= esc_attr( $current_html ); ?>">
+            <label for="{$title_id}">タイトル:</label> 
+            <input class="widefat" id="{$title_id}" name="{$title_name}" type="text" value="{$title}">
         </p>
-        <?php
+        <p>
+            <label for="{$description_id}">商品説明:</label> 
+            <input class="widefat" id="{$description_id}" name="{$description_name}" type="text" value="{$description}">
+        </p>
+___HTML___;
     }
 
     /**
-     * ウィジェットオプションの保存処理.
+     * 商品登録ウィジェットオプションの保存処理
      * 
      * @param array $new_instance 保存用に送信された値.
      * @param array $old_instance データベースからの以前保存された値.
@@ -61,14 +70,17 @@ class Product_Widget extends WP_Widget {
      * @return array 保存される更新された安全な値.
      */
     public function update( $new_instance, $old_instance ) {
-        $instance = array();
+        // インスタンス変数の初期化
+        $instance = [];
+        // タイトルの格納
         $instance['title'] = !empty( $new_instance['title'] ) ? strip_tags( $new_instance['title'] ) : '';
-        $instance['setting_html'] = !empty( $new_instance['setting_html'] ) ? $new_instance['setting_html'] : '';
-
+        // 詳細の格納
+        $instance['description'] = !empty( $new_instance['description'] ) ? strip_tags( $new_instance['description'] ) : '';        
+        // 連想配列を返す
         return $instance;
     }
 }
 
 add_action( 'widgets_init', function() {
     register_widget( 'Product_Widget' );
-} );
+});
